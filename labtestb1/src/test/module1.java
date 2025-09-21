3452657cb4384b4c31ca4248d2f42981adc2233a
Rockstatata <sarwad015@gmail.com>
2025-09-21 11:25:54 +0600

    add attri

--- labtestb1/src/test/module1.java
+++ labtestb1/src/test/module1.java
@@ -2,6 +2,8 @@ package test;
 
 public class module1 implements modules {
 
+    public String duration;
+
     @Override
     public void showdetails() {
         System.out.println("This is module 1");
